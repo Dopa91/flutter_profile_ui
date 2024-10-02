24623e6f6f34a2b48f9b50cf4f8ea9da80ea395b
Dopa91 <code.dopatka@gmail.com>
2024-10-02 16:22:27 +0200

    class Fairytail Char

---
+++ lib/fairytail_char.dart
@@ -0,0 +1,23 @@
+class FairytailChar {
+  final String name;
+  final String occupation;
+  final String birthday;
+  final int age;
+  final String guild;
+  final String magic;
+  final String loves;
+  final String team;
+  final int battles;
+
+  FairytailChar({
+    required this.battles,
+    required this.magic,
+    required this.loves,
+    required this.team,
+    required this.name,
+    required this.occupation,
+    required this.birthday,
+    required this.age,
+    required this.guild,
+  });
+}
