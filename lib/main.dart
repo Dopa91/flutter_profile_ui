2c26f86e3f7e440d6fab3160fe5b245872d0e19f
Dopa91 <code.dopatka@gmail.com>
2024-10-02 21:39:57 +0200

    created new widget, corrected some issues

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 //import 'dart:html';
 
 import 'package:flutter/material.dart';
-import 'package:miniprofile/custom_profile_erza.dart';
+import 'package:miniprofile/custom_profile.dart';
 import 'package:miniprofile/custom_profile_happy.dart';
 import 'package:miniprofile/fairytail_char_memberlist.dart';
 import 'package:miniprofile/info_box_item.dart';
@@ -24,8 +24,8 @@ int counter = 0;
 
 class _ProfileState extends State<Profile> {
   final List<Widget> widgets = [
-    CustomProfileSiteErza(counter: counter, profile: memberList[1]),
-    CustomProfileSiteHappy(counter: counter, profile: memberList[0])
+    CustomProfileSite(counter: counter, profile: memberList[1]),
+    CustomProfileSite(counter: counter, profile: memberList[0])
   ];
 
   int currentIndex = 0;
@@ -44,25 +44,35 @@ class _ProfileState extends State<Profile> {
               icon: Icon(Icons.catching_pokemon), label: "Happy"),
         ],
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          setState(() {
-            counter = counter + 1;
-          });
-        },
-        shape: CircleBorder(),
-        child: Container(
-          width: 60,
-          height: 60,
-          child: Icon(Icons.add),
-          decoration: BoxDecoration(
-              shape: BoxShape.circle,
-              gradient: LinearGradient(
-                colors: [Colors.deepPurple.shade800, Colors.deepPurpleAccent],
-              )),
-        ),
-      ),
+      floatingActionButton: FairyTailFab(onTap: () {}),
       body: widgets[currentIndex],
+    );
+  }
+}
+
+class FairyTailFab extends StatelessWidget {
+  const FairyTailFab({
+    super.key,
+    required this.onTap,
+  });
+
+  final VoidCallback onTap;
+
+  @override
+  Widget build(BuildContext context) {
+    return FloatingActionButton(
+      onPressed: onTap,
+      shape: CircleBorder(),
+      child: Container(
+        width: 60,
+        height: 60,
+        child: Icon(Icons.add),
+        decoration: BoxDecoration(
+            shape: BoxShape.circle,
+            gradient: LinearGradient(
+              colors: [Colors.deepPurple.shade800, Colors.deepPurpleAccent],
+            )),
+      ),
     );
   }
 }
