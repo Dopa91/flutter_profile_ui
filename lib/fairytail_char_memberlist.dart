6150022a093c5ddb275e1bc16872b08d304a0909
Dopa91 <code.dopatka@gmail.com>
2024-10-02 16:22:51 +0200

    objekt from class FairytailChar

---
+++ lib/fairytail_char_memberlist.dart
@@ -0,0 +1,15 @@
+import 'package:miniprofile/fairytail_char.dart';
+
+final List<FairytailChar> memberList = [
+  FairytailChar(
+    battles: 420,
+    magic: "Aera, Transformation Magic, Fire Magic & Ushi no Koku Mairi",
+    loves: "Fish 'yummy'",
+    team: "Team Natsu",
+    name: "Happy",
+    occupation: "Mage",
+    birthday: "June 20",
+    age: 6,
+    guild: "FairyTail",
+  )
+];
