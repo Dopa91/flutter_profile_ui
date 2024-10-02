ffbd7e9145c663f9c879474cd84566d6d5c58448
Dopa91 <code.dopatka@gmail.com>
2024-10-02 21:03:23 +0200

    added profiels to list

--- lib/fairytail_char_memberlist.dart
+++ lib/fairytail_char_memberlist.dart
@@ -1,9 +1,10 @@
 import 'package:miniprofile/fairytail_char.dart';
+import 'package:miniprofile/main.dart';
 
 final List<FairytailChar> memberList = [
   FairytailChar(
-    battles: 420,
-    magic: "Aera, Transformation Magic, Fire Magic & Ushi no Koku Mairi",
+    battles: counter,
+    magic: "Aera, Transformation & Fire Magic",
     loves: "Fish 'yummy'",
     team: "Team Natsu",
     name: "Happy",
@@ -11,5 +12,18 @@ final List<FairytailChar> memberList = [
     birthday: "June 20",
     age: 6,
     guild: "FairyTail",
-  )
+    image: "assets/happy.jpg",
+  ),
+  FairytailChar(
+    battles: counter,
+    magic: "Spatial & Sword Magic, Telekinesis",
+    loves: "Eating cakes",
+    team: "Team Natsu",
+    name: "Erza Scarlet",
+    occupation: "S Class Mage",
+    birthday: "April 7th",
+    age: 19,
+    guild: "FairyTail, Magnolia",
+    image: "assets/erza.jpg",
+  ),
 ];
